6d1beb16baa23067558b8c0b48b6dffc1c31c3e9
Daniel Pope <danielthepope@users.noreply.github.com>
2017-01-10 00:32:11 +0000

    Made settings persist

--- Pensieve/Properties/Settings.Designer.cs
+++ Pensieve/Properties/Settings.Designer.cs
@@ -8,25 +8,31 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Pensieve.Properties
-{
-
-
+namespace Pensieve.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
             }
         }
-
-        public string LibraryPath { get; internal set; }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string LibraryPath {
+            get {
+                return ((string)(this["LibraryPath"]));
+            }
+            set {
+                this["LibraryPath"] = value;
+            }
+        }
     }
 }
